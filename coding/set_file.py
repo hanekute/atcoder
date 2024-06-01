b6a783ddbbee9e22f8603e6667ec1581cf76d570
hanehane <hanehaneac@gmail.com>
2024-06-01 20:57:32 +0900

    set_fileのパスを修正

--- coding/set_file.py
+++ coding/set_file.py
@@ -1,5 +1,5 @@
 for problem_number in ['A', 'B', 'C', 'D', 'E', 'F', 'G']:
-    path = 'contest/'+ problem_number + '.py'
+    path = problem_number + '.py'
 
     with open(path, mode='w') as f:
         f.write('')
